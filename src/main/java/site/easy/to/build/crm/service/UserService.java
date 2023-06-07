83c68a9b0d70487cd66a460af41965d22d4f8908
Ahmed <ahmed.samir.31992@gmail.com>
2023-06-07 19:37:46 +0300

    Add UserService interface with user management methods

---
+++ src/main/java/site/easy/to/build/crm/service/UserService.java
@@ -0,0 +1,18 @@
+package site.easy.to.build.crm.service;
+
+import site.easy.to.build.crm.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    public User findById(int id);
+
+    public List<User> findByUsername(String username);
+
+
+    public void save(User user);
+
+    public void deleteById(int id);
+
+}
