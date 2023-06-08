001ee5e17d31ccef58294162c5d4320d9776e712
Ahmed <ahmed.samir.31992@gmail.com>
2023-06-08 10:18:10 +0300

    Implement UserService with CRUD operations for User entity
    In this commit, a new UserServiceImpl class has been added, implementing the UserService interface. The UserServiceImpl class provides CRUD operations for the User entity.

---
+++ src/main/java/site/easy/to/build/crm/service/UserServiceImpl.java
@@ -0,0 +1,35 @@
+package site.easy.to.build.crm.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import site.easy.to.build.crm.dao.UserRepository;
+import site.easy.to.build.crm.entity.User;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public User findById(int id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public List<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        userRepository.deleteById(id);
+    }
+}
