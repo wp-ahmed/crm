7ebd861b71c1689de38446db765b71d536d3cba3
Ahmed <ahmed.samir.31992@gmail.com>
2023-06-12 18:16:44 +0300

    Create CustomOAuth2UserService for loading custom OAuth2 users

---
+++ src/main/java/site/easy/to/build/crm/config/oauth2/CustomOAuth2UserService.java
@@ -0,0 +1,21 @@
+package site.easy.to.build.crm.config.oauth2;
+
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+
+@Service
+//defaultOidcUser
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        DefaultOidcUser defaultOidcUser = (DefaultOidcUser) super.loadUser(userRequest);
+        return new CustomOAuth2User(defaultOidcUser);
+    }
+
+}
