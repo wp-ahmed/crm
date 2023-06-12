e3b4b39c4f9f953ee552a5fc9951967dca9903c8
Ahmed <ahmed.samir.31992@gmail.com>
2023-06-12 18:18:02 +0300

    Create OAuthLoginSuccessHandler for handling successful OAuth2 logins

---
+++ src/main/java/site/easy/to/build/crm/config/oauth2/OAuthLoginSuccessHandler.java
@@ -0,0 +1,100 @@
+package site.easy.to.build.crm.config.oauth2;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.OAuth2RefreshToken;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import site.easy.to.build.crm.entity.OAuthUser;
+import site.easy.to.build.crm.entity.User;
+import site.easy.to.build.crm.service.OAuthUserService;
+import site.easy.to.build.crm.service.UserService;
+
+import java.io.IOException;
+
+@Component
+public class OAuthLoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    @Autowired
+    public OAuthUserService oAuthUserService;
+
+    @Autowired
+    public UserService userService;
+
+    @Autowired
+    private OAuth2AuthorizedClientService authorizedClientService;
+
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        // Get the registration ID of the OAuth2 provider
+        DefaultOidcUser oidcUser = (DefaultOidcUser) authentication.getPrincipal();
+
+        // Get the registration ID of the OAuth2 provider
+        OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
+        String registrationId = oauthToken.getAuthorizedClientRegistrationId();
+
+        if (registrationId == null) {
+            // Handle the case when the registrationId is not found
+            throw new ServletException("Failed to find the registrationId from the authorities");
+        }
+
+
+
+        // Obtain the OAuth2AuthorizedClient
+        OAuth2AuthorizedClient authorizedClient = authorizedClientService.loadAuthorizedClient(registrationId, authentication.getName());
+
+
+
+        // Get the access and the refresh token from the OAuth2AuthorizedClient
+
+        OAuth2AccessToken oAuth2AccessToken = authorizedClient.getAccessToken();
+        OAuth2RefreshToken oAuth2RefreshToken = authorizedClient.getRefreshToken();
+
+        String email = ((DefaultOidcUser) authentication.getPrincipal()).getEmail();
+        String name = email.split("@")[0];
+        String username = email.split("@")[0];
+
+        User user = userService.findByEmail(email);
+        OAuthUser oAuthUser;
+
+        if(user == null) {
+            user = new User();
+            user.setEmail(email);
+            user.setUsername(username);
+            user.setFirstName(name);
+            user.setStatus(User.Status.ACTIVE);
+            user.setDepartment("client");
+            userService.save(user);
+            oAuthUser = new OAuthUser();
+            updateOAuthUserTokens(oAuthUser, oAuth2AccessToken, oAuth2RefreshToken);
+        }else{
+            oAuthUser = user.getOauthUser();
+            updateOAuthUserTokens(oAuthUser,oAuth2AccessToken,oAuth2RefreshToken);
+        }
+
+        oAuthUserService.save(oAuthUser,user);
+
+        response.sendRedirect("/register");
+    }
+
+    private void updateOAuthUserTokens(OAuthUser oAuthUser,OAuth2AccessToken oAuth2AccessToken, OAuth2RefreshToken oAuth2RefreshToken) {
+        oAuthUser.setAccessToken(oAuth2AccessToken.getTokenValue());
+        oAuthUser.setAccessTokenIssuedAt(oAuth2AccessToken.getIssuedAt());
+        oAuthUser.setAccessTokenExpiration(oAuth2AccessToken.getExpiresAt());
+
+        if(oAuth2RefreshToken != null) {
+            oAuthUser.setRefreshToken(oAuth2RefreshToken.getTokenValue());
+            oAuthUser.setRefreshTokenIssuedAt(oAuth2RefreshToken.getIssuedAt());
+            oAuthUser.setRefreshTokenExpiration(oAuth2RefreshToken.getExpiresAt());
+        }
+    }
+}
