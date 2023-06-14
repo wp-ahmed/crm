2d88f88673485083feb5d201765e2d3cebe2c2e8
Ahmed <ahmed.samir.31992@gmail.com>
2023-06-14 18:03:07 +0300

    Add Google Calendar EventDateTime DAO class
    This commit introduces the EventDateTime data access object (DAO) class, which represents the start and end times of Google Calendar events within the application.

---
+++ src/main/java/site/easy/to/build/crm/google/dao/EventDateTime.java
@@ -0,0 +1,13 @@
+package site.easy.to.build.crm.google.dao;
+
+public class EventDateTime {
+    private String dateTime;
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+}
