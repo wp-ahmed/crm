b1a5056806bd539d8e10078bf44a1bb69cf3afb1
Ahmed <ahmed.samir.31992@gmail.com>
2023-06-14 18:21:55 +0300

    Implement refreshAccessTokenIfNeeded and getOAuthUserByUser in OAuthUserServiceImpl
    This commit updates the OAuthUserServiceImpl class to include the implementation of the new methods getOAuthUserByUser and refreshAccessTokenIfNeeded.

--- src/main/java/site/easy/to/build/crm/service/OAuthUserServiceImpl.java
+++ src/main/java/site/easy/to/build/crm/service/OAuthUserServiceImpl.java
@@ -1,16 +1,28 @@
 package site.easy.to.build.crm.service;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleRefreshTokenRequest;
+import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import site.easy.to.build.crm.dao.OAuthUserRepository;
 import site.easy.to.build.crm.dao.UserRepository;
 import site.easy.to.build.crm.entity.OAuthUser;
 import site.easy.to.build.crm.entity.User;
 
-import java.security.Principal;
+import java.io.IOException;
+import java.time.Instant;
 
 @Service
 public class OAuthUserServiceImpl implements OAuthUserService{
+
+    @Value("${spring.security.oauth2.client.registration.google.client-id}")
+    private String clientId;
+
+    @Value("${spring.security.oauth2.client.registration.google.client-secret}")
+    private String clientSecret;
 
     @Autowired
     OAuthUserRepository oAuthUserRepository;
@@ -24,10 +36,48 @@ public class OAuthUserServiceImpl implements OAuthUserService{
     }
 
     @Override
+    public OAuthUser getOAuthUserByUser(User user) {
+        return oAuthUserRepository.getOAuthUserByUser(user);
+    }
+
+    @Override
+    public String refreshAccessTokenIfNeeded(OAuthUser oauthUser) {
+        Instant now = Instant.now();
+        if (now.isBefore(oauthUser.getAccessTokenExpiration())) {
+            return oauthUser.getAccessToken();
+        }
+
+        GsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+
+        // Create a new GoogleTokenResponse
+        GoogleTokenResponse tokenResponse;
+        try {
+            tokenResponse = new GoogleRefreshTokenRequest(
+                    new NetHttpTransport(),
+                    jsonFactory,
+                    oauthUser.getRefreshToken(),
+                    clientId,
+                    clientSecret)
+                    .execute();
+            String newAccessToken = tokenResponse.getAccessToken();
+            long expiresIn = tokenResponse.getExpiresInSeconds();
+            Instant expiresAt = Instant.now().plusSeconds(expiresIn);
+
+            oauthUser.setAccessToken(newAccessToken);
+            oauthUser.setAccessTokenExpiration(expiresAt);
+
+            oAuthUserRepository.save(oauthUser);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return oauthUser.getAccessToken();
+    }
+
+    @Override
     public void save(OAuthUser oAuthUser, User user) {
         oAuthUser.setUser(user);
         user.setOauthUser(oAuthUser);
-//        userRepository
         oAuthUserRepository.save(oAuthUser);
     }
 
@@ -35,4 +85,5 @@ public class OAuthUserServiceImpl implements OAuthUserService{
     public void deleteById(int id) {
 
     }
+
 }
