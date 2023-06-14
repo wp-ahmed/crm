f58cb33771c746bd6841f04bb4e0dcca87a1be2a
Ahmed <ahmed.samir.31992@gmail.com>
2023-06-14 18:02:19 +0300

    Add Google Calendar EventList DAO class
    This commit introduces the EventList data access object (DAO) class, which represents a list of Google Calendar events within the application.

---
+++ src/main/java/site/easy/to/build/crm/google/dao/EventList.java
@@ -0,0 +1,17 @@
+package site.easy.to.build.crm.google.dao;
+
+import java.util.List;
+
+public class EventList {
+
+    private List<Event> items;
+
+
+    public List<Event> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Event> items) {
+        this.items = items;
+    }
+}
