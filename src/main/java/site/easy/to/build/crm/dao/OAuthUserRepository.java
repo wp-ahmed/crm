1b8fc42d65c10bffa6302ff9d9c9d0bb80eeb799
Ahmed <ahmed.samir.31992@gmail.com>
2023-06-14 18:19:06 +0300

    Add getOAuthUserByUser method to OAuthUserRepository
    This commit updates the OAuthUserRepository interface to include a new method, getOAuthUserByUser, which retrieves an OAuthUser entity based on a given User entity.

--- src/main/java/site/easy/to/build/crm/dao/OAuthUserRepository.java
+++ src/main/java/site/easy/to/build/crm/dao/OAuthUserRepository.java
@@ -3,11 +3,14 @@ package site.easy.to.build.crm.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import site.easy.to.build.crm.entity.OAuthUser;
+import site.easy.to.build.crm.entity.User;
 
 @Repository
 public interface OAuthUserRepository extends JpaRepository<OAuthUser,Integer> {
 
     public OAuthUser findById(int id);
+
+    public OAuthUser getOAuthUserByUser(User user);
 
     public void deleteById(int id);
 }
