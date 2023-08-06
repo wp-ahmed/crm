6a983a2ff1930709acc8554deefd4894bf7a5723
Ahmed <ahmed.samir.31992@gmail.com>
2023-08-06 08:10:15 +0300

    Create Attachment class for Gmail email attachments The class Attachment is created to represent Gmail email attachments.

---
+++ src/main/java/site/easy/to/build/crm/google/model/gmail/Attachment.java
@@ -0,0 +1,50 @@
+package site.easy.to.build.crm.google.model.gmail;
+
+public class Attachment {
+    private String name;
+    private String data;
+    private String mimeType;
+    private int size;
+
+    public Attachment() {
+    }
+
+    public Attachment(String name, String data, String mimeType, int size) {
+        this.name = name;
+        this.data = data;
+        this.mimeType = mimeType;
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getMimeType() {
+        return mimeType;
+    }
+
+    public void setMimeType(String mimeType) {
+        this.mimeType = mimeType;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+}
