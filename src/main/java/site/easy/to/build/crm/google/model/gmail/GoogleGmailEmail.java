f8cd750b14772c61f8d081c8641f7ad654ba653e
Ahmed <ahmed.samir.31992@gmail.com>
2023-08-06 16:42:03 +0300

    Implement GoogleGmailEmail for representing the Gmail email API response

---
+++ src/main/java/site/easy/to/build/crm/google/model/gmail/GoogleGmailEmail.java
@@ -0,0 +1,23 @@
+package site.easy.to.build.crm.google.model.gmail;
+
+public class GoogleGmailEmail {
+
+    private String id;
+    private String threadId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getThreadId() {
+        return threadId;
+    }
+
+    public void setThreadId(String threadId) {
+        this.threadId = threadId;
+    }
+}
