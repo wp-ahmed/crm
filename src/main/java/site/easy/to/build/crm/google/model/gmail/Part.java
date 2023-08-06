d3232800269fdc3ef84d8303a7627d167cda703c
Ahmed <ahmed.samir.31992@gmail.com>
2023-08-06 17:04:31 +0300

    Add Part class for representing Gmail message parts and attachments.

---
+++ src/main/java/site/easy/to/build/crm/google/model/gmail/Part.java
@@ -0,0 +1,43 @@
+package site.easy.to.build.crm.google.model.gmail;
+
+import java.util.List;
+
+public class Part {
+    private String mimeType;
+    private List<CustomHeader> headers;
+    private Body body;
+    private List<Part> parts;
+
+    // Getters and setters
+    public String getMimeType() {
+        return mimeType;
+    }
+
+    public void setMimeType(String mimeType) {
+        this.mimeType = mimeType;
+    }
+
+    public List<CustomHeader> getHeaders() {
+        return headers;
+    }
+
+    public void setHeaders(List<CustomHeader> headers) {
+        this.headers = headers;
+    }
+
+    public Body getBody() {
+        return body;
+    }
+
+    public void setBody(Body body) {
+        this.body = body;
+    }
+
+    public List<Part> getParts() {
+        return parts;
+    }
+
+    public void setParts(List<Part> parts) {
+        this.parts = parts;
+    }
+}
