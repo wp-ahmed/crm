d92b458334bf9a055e44387d58ef658d77796db9
Ahmed <ahmed.samir.31992@gmail.com>
2023-08-06 09:46:16 +0300

    Add CustomHeader class for Gmail email API headers
    The CustomHeader class is included to represent custom headers for the Gmail email API

---
+++ src/main/java/site/easy/to/build/crm/google/model/gmail/CustomHeader.java
@@ -0,0 +1,23 @@
+package site.easy.to.build.crm.google.model.gmail;
+
+public class CustomHeader {
+    private String name;
+    private String value;
+
+    // Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
