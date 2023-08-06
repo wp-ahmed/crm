664d0e145fe6873dcaea732609e4b9e0fcc829fc
Ahmed <ahmed.samir.31992@gmail.com>
2023-08-06 16:30:55 +0300

    Implement GoogleAccessService for managing Google API access

---
+++ src/main/java/site/easy/to/build/crm/google/service/acess/GoogleAccessService.java
@@ -0,0 +1,33 @@
+package site.easy.to.build.crm.google.service.acess;
+
+import org.springframework.security.core.Authentication;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.servlet.view.RedirectView;
+import site.easy.to.build.crm.entity.OAuthUser;
+import site.easy.to.build.crm.entity.User;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public interface GoogleAccessService {
+
+    static final String SCOPE_CALENDAR = "https://www.googleapis.com/auth/calendar";
+    static final String SCOPE_GMAIL = "https://www.googleapis.com/auth/gmail.modify";
+    static final String SCOPE_DRIVE = "https://www.googleapis.com/auth/drive.file";
+    static final String REDIRECT_URI = "http://localhost:8080/settings/handle-granted-access";
+
+    public RedirectView grantGoogleAccess(Authentication authentication,
+                                          HttpSession session,
+                                          boolean grantCalendarAccess,
+                                          boolean grantGmailAccess,
+                                          boolean grantDriveAccess);
+    public String handleGrantedAccess(HttpSession session,
+                                      String error,
+                                      String authCode,
+                                      String state,
+                                      Authentication authentication) throws IOException;
+
+    public void verifyAccessAndHandleRevokedToken(OAuthUser oAuthUser, User user, List<String> scopesToCheck) throws IOException;
+
+}
