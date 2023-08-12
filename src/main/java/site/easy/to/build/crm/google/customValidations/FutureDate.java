c98817531cc78efffc3807ac2c3a9685cf70a3d6
Ahmed <ahmed.samir.31992@gmail.com>
2023-08-13 01:26:41 +0300

    Add custom annotation validation for validating the date that entered by the user not in the past.

---
+++ src/main/java/site/easy/to/build/crm/google/customValidations/FutureDate.java
@@ -0,0 +1,20 @@
+package site.easy.to.build.crm.google.customValidations;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = FutureDateValidator.class)
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface FutureDate {
+    String message() default "Date must be in the future";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ src/main/java/site/easy/to/build/crm/google/customValidations/FutureDateValidator.java
@@ -0,0 +1,26 @@
+package site.easy.to.build.crm.google.customValidations;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+public class FutureDateValidator implements ConstraintValidator<FutureDate, String> {
+    @Override
+    public void initialize(FutureDate constraintAnnotation) {
+        // No initialization needed
+    }
+
+    @Override
+    public boolean isValid(String dateString, ConstraintValidatorContext context) {
+        if (dateString == null || dateString.isEmpty()) {
+            return false;
+        }
+
+        try {
+            LocalDate date = LocalDate.parse(dateString);
+            return date.isAfter(LocalDate.now()); // Check if the date is in the future
+        } catch (DateTimeParseException e) {
+            return false; // Invalid date format
+        }
+    }
+}
