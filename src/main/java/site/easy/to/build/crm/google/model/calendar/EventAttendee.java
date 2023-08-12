225f023ee2e0b183fc2b849e05877096becd6b6a
Ahmed <ahmed.samir.31992@gmail.com>
2023-08-13 00:52:07 +0300

    Added EventAttendee to hold the information of the people who will be invited by the user to his calendar event.

---
+++ src/main/java/site/easy/to/build/crm/google/model/calendar/EventAttendee.java
@@ -0,0 +1,13 @@
+package site.easy.to.build.crm.google.model.calendar;
+
+public class EventAttendee {
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
