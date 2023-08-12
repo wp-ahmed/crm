adec2d1363fdbba432f59cc201bb316b3fbfc0ff
Ahmed <ahmed.samir.31992@gmail.com>
2023-08-13 01:03:51 +0300

    Added custom annotation validation for validation that the created event had been created at the same day.

---
+++ src/main/java/site/easy/to/build/crm/google/customValidations/SameDay.java
@@ -0,0 +1,20 @@
+package site.easy.to.build.crm.google.customValidations;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = SameDayValidator.class)
+public @interface SameDay {
+    String message() default "Start and end dates must be equal and on the same day";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ src/main/java/site/easy/to/build/crm/google/customValidations/SameDayValidator.java
@@ -0,0 +1,32 @@
+package site.easy.to.build.crm.google.customValidations;
+
+import site.easy.to.build.crm.google.model.calendar.EventDisplay;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.time.LocalDate;
+
+public class SameDayValidator implements ConstraintValidator<SameDay, EventDisplay> {
+    @Override
+    public void initialize(SameDay constraintAnnotation) {
+        // No initialization needed
+    }
+
+    @Override
+    public boolean isValid(EventDisplay eventDisplay, ConstraintValidatorContext context) {
+        if (eventDisplay == null) {
+            return true; // Skip validation if object is null
+        }
+        if(eventDisplay.getStartDate() == null || eventDisplay.getStartDate().isEmpty()){
+            return true;
+        }
+        if(eventDisplay.getEndDate() == null || eventDisplay.getEndDate().isEmpty()){
+            return true;
+        }
+        LocalDate startDate = LocalDate.parse(eventDisplay.getStartDate());
+        LocalDate endDate = LocalDate.parse(eventDisplay.getEndDate());
+
+        return startDate.isEqual(endDate);
+    }
+}
