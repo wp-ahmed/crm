4b8df26a2b84fe960547dfbaab3bc7ba82edf640
Ahmed <ahmed.samir.31992@gmail.com>
2023-08-13 00:32:26 +0300

    Added custom validation annotation 'EndTimeAfterStartTime' for validating the range of the selected time.

---
+++ src/main/java/site/easy/to/build/crm/google/customValidations/EndTimeAfterStartTime.java
@@ -0,0 +1,21 @@
+package site.easy.to.build.crm.google.customValidations;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = EndTimeAfterStartTimeValidator.class)
+public @interface EndTimeAfterStartTime {
+
+    String message() default "End time must be after start time";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ src/main/java/site/easy/to/build/crm/google/customValidations/EndTimeAfterStartTimeValidator.java
@@ -0,0 +1,27 @@
+package site.easy.to.build.crm.google.customValidations;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import site.easy.to.build.crm.google.model.calendar.EventDisplay;
+
+import java.time.LocalTime;
+
+public class EndTimeAfterStartTimeValidator implements ConstraintValidator<EndTimeAfterStartTime, EventDisplay> {
+
+    @Override
+    public void initialize(EndTimeAfterStartTime constraintAnnotation) {
+    }
+
+    @Override
+    public boolean isValid(EventDisplay event, ConstraintValidatorContext context) {
+        if (event == null) {
+            return true; // null values are considered valid
+        }
+        if(event.getEndTime() == null || event.getEndTime().isEmpty() || event.getStartTime() == null || event.getStartTime().isEmpty()){
+            return true;
+        }
+        LocalTime startTime = LocalTime.parse(event.getStartTime());
+        LocalTime endTime = LocalTime.parse(event.getEndTime());
+        return endTime.isAfter(startTime);
+    }
+}
