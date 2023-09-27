e262e910dd03453ca783681310e16f339889b025
Ahmed <ahmed.samir.31992@gmail.com>
2023-09-27 19:47:49 +0300

    Implement WeatherService to handle the API call of the Weather service.

---
+++ src/main/java/site/easy/to/build/crm/service/weather/WeatherService.java
@@ -0,0 +1,26 @@
+package site.easy.to.build.crm.service.weather;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import site.easy.to.build.crm.entity.WeatherData;
+
+@Service
+public class WeatherService {
+    private final RestTemplate restTemplate;
+    private final String weatherApiUrl;
+    private final String apiKey;
+
+    public WeatherService(RestTemplate restTemplate,
+                          @Value("${weather.api.url}") String weatherApiUrl,
+                          @Value("${weather.api.key}") String apiKey) {
+        this.restTemplate = restTemplate;
+        this.weatherApiUrl = weatherApiUrl;
+        this.apiKey = apiKey;
+    }
+
+    public WeatherData getWeatherData(String city) {
+        String apiUrl = weatherApiUrl + "?key=" + apiKey + "&q=" + city;
+        return restTemplate.getForObject(apiUrl, WeatherData.class);
+    }
+}
