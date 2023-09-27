86a2bd807f78187d12b28e406537518c09a7e8e1
Ahmed <ahmed.samir.31992@gmail.com>
2023-09-27 08:59:18 +0300

    "Implement additional features in CrmUserDetails
    This commit adds new features to the CrmUserDetails service, enhancing its functionality and security:
    
    User Suspension Check: The service now checks if a user is suspended before allowing access to the system. If a user is suspended, access is denied to ensure system integrity and user compliance.
    
    Role-Based Access Control: The logged-in user now has assigned roles that determine their access privileges within the system. This feature provides fine-grained control over what resources and functionalities a user can access, enhancing security and enforcing appropriate permissions.

--- src/main/java/site/easy/to/build/crm/config/CrmUserDetails.java
+++ src/main/java/site/easy/to/build/crm/config/CrmUserDetails.java
@@ -1,16 +1,23 @@
 package site.easy.to.build.crm.config;
 
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import site.easy.to.build.crm.dao.UserRepository;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import site.easy.to.build.crm.repository.UserRepository;
 import site.easy.to.build.crm.entity.User;
 
-import java.util.ArrayList;
+import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 public class CrmUserDetails implements UserDetailsService {
@@ -18,17 +25,33 @@ public class CrmUserDetails implements UserDetailsService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    HttpSession session;
+    @Autowired
+    private HttpServletResponse request;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        String crmUsername, password = null;
-        List<GrantedAuthority> authorities = null;
-        List<User> user = userRepository.findByUsername(username);
-        if(user.size() == 0) {
+        String crmUsername, password;
+        User user = userRepository.findByUsername(username).size() == 1  ? userRepository.findByUsername(username).get(0) : null;
+        List<GrantedAuthority> authorities;
+        if(user == null) {
             throw new UsernameNotFoundException("user details not found for the user : " + username);
         } else {
-            crmUsername = user.get(0).getUsername();
-            password = user.get(0).getPassword();
-            authorities = new ArrayList<>();
+            if(user.getStatus().equals("suspended")) {
+                HttpServletResponse httpServletResponse =
+                        ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())).getResponse();
+                try {
+                    assert httpServletResponse != null;
+                    httpServletResponse.sendRedirect("/account-suspended");
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            password = user.getPassword();
+            session.setAttribute("loggedInUserId", user.getId());
+            authorities = user.getRoles().stream()
+                    .map(role -> new SimpleGrantedAuthority(role.getName()))
+                    .collect(Collectors.toList());
         }
 
         return new org.springframework.security.core.userdetails.User(username,password,authorities);
