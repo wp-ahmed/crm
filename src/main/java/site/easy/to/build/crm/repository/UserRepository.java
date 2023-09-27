854bc12d130ab5f4aef60cba11364af8fec6e1b0
Ahmed <ahmed.samir.31992@gmail.com>
2023-09-27 19:44:03 +0300

    Add more functionalities to UserRepository to get and count all the current users in the system.

--- src/main/java/site/easy/to/build/crm/repository/UserRepository.java
+++ src/main/java/site/easy/to/build/crm/repository/UserRepository.java
@@ -1,5 +1,6 @@
-package site.easy.to.build.crm.dao;
+package site.easy.to.build.crm.repository;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import site.easy.to.build.crm.entity.User;
@@ -7,7 +8,7 @@ import site.easy.to.build.crm.entity.User;
 import java.util.List;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
     public User findById(int id);
 
@@ -16,4 +17,12 @@ public interface UserRepository extends JpaRepository<User,Integer> {
     public User findByEmail(String email);
 
     public void deleteById(int id);
+
+    public List<User> findAll();
+
+    public User findByToken(String token);
+
+    long count();
+
+    public List<User> findTopNByOrderByCreatedAtDesc(int limit, Pageable pageable);
 }
