590a191125bd66a7100fad6b629539dec7259a98
Ahmed <ahmed.samir.31992@gmail.com>
2023-09-28 08:27:11 +0300

    Implement CustomerLoginController for customer login and password management

---
+++ src/main/java/site/easy/to/build/crm/controller/CustomerLoginController.java
@@ -0,0 +1,61 @@
+package site.easy.to.build.crm.controller;
+
+import jakarta.annotation.Nullable;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import site.easy.to.build.crm.entity.Customer;
+import site.easy.to.build.crm.entity.CustomerLoginInfo;
+import site.easy.to.build.crm.service.customer.CustomerLoginInfoService;
+import site.easy.to.build.crm.service.customer.CustomerService;
+
+@Controller
+public class CustomerLoginController {
+
+    private final CustomerLoginInfoService customerLoginInfoService;
+    private final CustomerService customerService;
+    private final PasswordEncoder passwordEncoder;
+
+    public CustomerLoginController(CustomerLoginInfoService customerLoginInfoService, CustomerService customerService, PasswordEncoder passwordEncoder) {
+        this.customerLoginInfoService = customerLoginInfoService;
+        this.customerService = customerService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+//
+    @GetMapping("/set-password")
+    public String showPasswordForm(Model model, @RequestParam("token") @Nullable String token) {
+        if(token == null) {
+            return "redirect:/set-password";
+        }
+        CustomerLoginInfo customerLoginInfo = customerLoginInfoService.findByToken(token);
+        if(customerLoginInfo == null) {
+            return "redirect:/set-password";
+        }
+        model.addAttribute("customerLoginInfo", customerLoginInfo);
+        return "set-password";
+    }
+
+    @PostMapping("/set-password")
+    public String setPassword(@ModelAttribute("customerLoginInfo") CustomerLoginInfo customerLoginInfo, @RequestParam("token") @Nullable String token) {
+        if(token ==null){
+            return "redirect:/set-password";
+        }
+        CustomerLoginInfo customerLoginInfo1 = customerLoginInfoService.findByToken(token);
+        if(customerLoginInfo1 == null) {
+            return "redirect:/set-password";
+        }
+        if(!customerLoginInfo1.isPasswordSet()){
+            String hashPassword = passwordEncoder.encode(customerLoginInfo.getPassword());
+            customerLoginInfo1.setPassword(hashPassword);
+            customerLoginInfo1.setPasswordSet(true);
+            customerLoginInfoService.save(customerLoginInfo1);
+        }
+        return "redirect:/customer-login";
+    }
+    @RequestMapping("/customer-login")
+    public String showCustomerLoginForm() {
+        return "customer-login";
+    }
+}
