c0eed5446f6463226a5d129191d8904b9469b04d
Ahmed <ahmed.samir.31992@gmail.com>
2023-09-28 21:27:09 +0300

    Create GoogleDriveFileService interface for managing Google Drive files.

---
+++ src/main/java/site/easy/to/build/crm/service/drive/GoogleDriveFileService.java
@@ -0,0 +1,14 @@
+package site.easy.to.build.crm.service.drive;
+
+import org.springframework.stereotype.Service;
+import site.easy.to.build.crm.entity.GoogleDriveFile;
+
+import java.util.List;
+
+public interface GoogleDriveFileService {
+    public List<GoogleDriveFile> getAllDriveFileByLeadId(int leadId);
+
+    public void save(GoogleDriveFile googleDriveFile);
+
+    public void delete(int id);
+}
