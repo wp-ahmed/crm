4600d2054100c857be64d713035fc82a5e80368f
Ahmed <ahmed.samir.31992@gmail.com>
2023-09-28 21:27:33 +0300

    Create GoogleDriveFileServiceImpl class for managing Google Drive files.

---
+++ src/main/java/site/easy/to/build/crm/service/drive/GoogleDriveFileServiceImpl.java
@@ -0,0 +1,35 @@
+package site.easy.to.build.crm.service.drive;
+
+import jakarta.validation.constraints.Null;
+import org.springframework.stereotype.Service;
+import site.easy.to.build.crm.entity.GoogleDriveFile;
+import site.easy.to.build.crm.repository.GoogleDriveFileRepository;
+
+import java.util.List;
+
+@Service
+public class GoogleDriveFileServiceImpl implements GoogleDriveFileService {
+
+    private final GoogleDriveFileRepository googleDriveFileRepository;
+
+    public GoogleDriveFileServiceImpl(GoogleDriveFileRepository googleDriveFileRepository) {
+        this.googleDriveFileRepository = googleDriveFileRepository;
+    }
+
+    @Override
+    public List<GoogleDriveFile> getAllDriveFileByLeadId(int leadId) {
+        return googleDriveFileRepository.findByLeadLeadId(leadId);
+    }
+
+    @Override
+    public void save(GoogleDriveFile googleDriveFile) {
+        googleDriveFileRepository.save(googleDriveFile);
+    }
+
+    @Override
+    public void delete(int id) {
+        if (googleDriveFileRepository.findById(id).isPresent()) {
+            googleDriveFileRepository.deleteById(id);
+        }
+    }
+}
