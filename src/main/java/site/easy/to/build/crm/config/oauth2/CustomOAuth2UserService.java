45a26811951a193b823152f9fd5076a494eff246
Ahmed <ahmed.samir.31992@gmail.com>
2023-09-28 08:50:05 +0300

    Extend OAuth2 user service with user repository integration

--- src/main/java/site/easy/to/build/crm/config/oauth2/CustomOAuth2UserService.java
+++ src/main/java/site/easy/to/build/crm/config/oauth2/CustomOAuth2UserService.java
@@ -6,16 +6,23 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import site.easy.to.build.crm.repository.UserRepository;
+import site.easy.to.build.crm.entity.User;
 
 
 @Service
-//defaultOidcUser
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    UserRepository userRepository;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         DefaultOidcUser defaultOidcUser = (DefaultOidcUser) super.loadUser(userRequest);
-        return new CustomOAuth2User(defaultOidcUser);
+
+        String email = defaultOidcUser.getEmail();
+        User user = userRepository.findByEmail(email);
+
+        return new CustomOAuth2User(defaultOidcUser, user);
     }
 
 }
