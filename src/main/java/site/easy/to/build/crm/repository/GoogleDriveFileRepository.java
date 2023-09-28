18ce1cf67542ecf509bc667c66a75c31f6a3f777
Ahmed <ahmed.samir.31992@gmail.com>
2023-09-28 21:26:06 +0300

    Create GoogleDriveFileRepository for accessing Google Drive files information.

---
+++ src/main/java/site/easy/to/build/crm/repository/GoogleDriveFileRepository.java
@@ -0,0 +1,14 @@
+package site.easy.to.build.crm.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import site.easy.to.build.crm.entity.GoogleDriveFile;
+
+import java.util.List;
+
+@Repository
+public interface GoogleDriveFileRepository extends JpaRepository<GoogleDriveFile, Integer> {
+    public List<GoogleDriveFile> findByLeadLeadId(int leadId);
+
+    void deleteById(int id);
+}
