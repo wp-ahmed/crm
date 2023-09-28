a4bde4a2d14a5fe21c5e9bc6e23215079e6c7859
Ahmed <ahmed.samir.31992@gmail.com>
2023-09-28 07:20:58 +0300

    Implement folder existence check in GoogleDriveApiService

--- src/main/java/site/easy/to/build/crm/google/service/drive/GoogleDriveApiService.java
+++ src/main/java/site/easy/to/build/crm/google/service/drive/GoogleDriveApiService.java
@@ -1,5 +1,6 @@
 package site.easy.to.build.crm.google.service.drive;
 
+import site.easy.to.build.crm.entity.File;
 import site.easy.to.build.crm.entity.OAuthUser;
 import site.easy.to.build.crm.google.model.drive.GoogleDriveFile;
 import site.easy.to.build.crm.google.model.drive.GoogleDriveFolder;
@@ -12,8 +13,11 @@ public interface GoogleDriveApiService {
     public List<GoogleDriveFile> listFiles(OAuthUser oAuthUser) throws IOException, GeneralSecurityException;
     public List<GoogleDriveFile> listFilesInFolder(OAuthUser oAuthUser, String folderId) throws IOException, GeneralSecurityException;
     public void createWorkspaceFile(OAuthUser oAuthUser, String name, String type) throws IOException, GeneralSecurityException;
+    public List<String> uploadWorkspaceFile(OAuthUser oAuthUser, List<File> files, String folderId) throws IOException, GeneralSecurityException;
 
-    public void createFolder(OAuthUser oAuthUser, String folderName) throws IOException, GeneralSecurityException;
+    public String createFolder(OAuthUser oAuthUser, String folderName) throws IOException, GeneralSecurityException;
+
+    void checkFolderExists(OAuthUser oAuthUser, String folderId) throws IOException, GeneralSecurityException;
 
     public void createFileInFolder(OAuthUser oAuthUser, String fileName, String folderId, String type) throws IOException, GeneralSecurityException;
 
@@ -24,4 +28,5 @@ public interface GoogleDriveApiService {
     public void findOrCreateTemplateFolder(OAuthUser oAuthUser, String folderName) throws IOException, GeneralSecurityException;
 
     public void deleteFile(OAuthUser oAuthUser, String fileId) throws IOException, GeneralSecurityException;
+    public boolean isFileExists(OAuthUser oAuthUser, String fileId) throws IOException, GeneralSecurityException;
 }
