7c6acd0ddac41635f090d270972316b0d38f5066
Ahmed <ahmed.samir.31992@gmail.com>
2023-09-30 08:25:20 +0300

    Refactor cal-init.js: Modify the url ajax requested links

---
+++ src/main/java/site/easy/to/build/crm/repository/TicketRepository.java
@@ -0,0 +1,34 @@
+package site.easy.to.build.crm.repository;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import site.easy.to.build.crm.entity.Customer;
+import site.easy.to.build.crm.entity.Ticket;
+
+import java.util.List;
+
+@Repository
+public interface TicketRepository extends JpaRepository<Ticket, Integer> {
+    public Ticket findByTicketId(int ticketId);
+
+    public List<Ticket> findByManagerId(int id);
+
+    public List<Ticket> findByEmployeeId(int id);
+
+    List<Ticket> findByCustomerCustomerId(Integer customerId);
+
+    List<Ticket> findByManagerIdOrderByCreatedAtDesc(int managerId, Pageable pageable);
+
+    List<Ticket> findByEmployeeIdOrderByCreatedAtDesc(int managerId, Pageable pageable);
+
+    List<Ticket> findByCustomerCustomerIdOrderByCreatedAtDesc(int customerId, Pageable pageable);
+
+    long countByEmployeeId(int employeeId);
+
+    long countByManagerId(int managerId);
+
+    long countByCustomerCustomerId(int customerId);
+
+    void deleteAllByCustomer(Customer customer);
+}
