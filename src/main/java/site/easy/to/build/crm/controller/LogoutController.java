44be6e0139bb9050a76f54ca3cac4e13f45ecbe2
Ahmed <ahmed.samir.31992@gmail.com>
2023-09-30 07:47:38 +0300

    Implement LogoutController for managing logout process.

---
+++ src/main/java/site/easy/to/build/crm/controller/LogoutController.java
@@ -0,0 +1,24 @@
+package site.easy.to.build.crm.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class LogoutController {
+
+    @PostMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+        return "redirect:/login";
+    }
+
+}
