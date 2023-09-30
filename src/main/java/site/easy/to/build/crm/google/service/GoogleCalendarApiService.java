8b283c32ad1b08e4b243f04d80590e109979dbf8
Ahmed Samir <54330098+wp-ahmed@users.noreply.github.com>
2023-09-30 08:49:42 +0300

    Update GoogleCalendarApiService.java

--- src/main/java/site/easy/to/build/crm/google/service/GoogleCalendarApiService.java
+++ src/main/java/site/easy/to/build/crm/google/service/GoogleCalendarApiService.java
@@ -1,13 +1,1 @@
-package site.easy.to.build.crm.google.service;
 
-import site.easy.to.build.crm.entity.OAuthUser;
-import site.easy.to.build.crm.google.dao.EventList;
-
-import java.io.IOException;
-import java.security.GeneralSecurityException;
-
-public interface GoogleCalendarApiService {
-
-    public EventList getEvents(String calendarId, OAuthUser oauthUser) throws IOException, GeneralSecurityException;
-
-}
