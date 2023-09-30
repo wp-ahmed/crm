9a409b746dc05f115ea754451bfdfd45d6d91d12
Ahmed <ahmed.samir.31992@gmail.com>
2023-09-30 08:29:25 +0300

    Create UniqueEmail custom validation annotation for users.

---
+++ src/main/java/site/easy/to/build/crm/customValidations/user/UniqueEmail.java
@@ -0,0 +1,20 @@
+package site.easy.to.build.crm.customValidations.user;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = UniqueEmailValidator.class)
+@Target({ElementType.FIELD, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface UniqueEmail {
+    String message() default "The email you entered is already registered. Please choose a different email address.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
