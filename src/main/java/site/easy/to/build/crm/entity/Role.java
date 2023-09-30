cd3bc5a73752f17b1f303f729a8c5f0347bbb9a4
Ahmed <ahmed.samir.31992@gmail.com>
2023-09-30 08:10:40 +0300

    Create Role entity for managing role information

---
+++ src/main/java/site/easy/to/build/crm/entity/Role.java
@@ -0,0 +1,38 @@
+package site.easy.to.build.crm.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Role() {
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
