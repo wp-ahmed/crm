9c329af65684734891d9d0034ba01c2183fb7b3f
Ahmed <ahmed.samir.31992@gmail.com>
2023-09-30 07:40:46 +0300

    Create LeadActionService interface for managing Lead Action information

---
+++ src/main/java/site/easy/to/build/crm/service/lead/LeadActionService.java
@@ -0,0 +1,7 @@
+package site.easy.to.build.crm.service.lead;
+
+import site.easy.to.build.crm.entity.LeadAction;
+
+public interface LeadActionService {
+    public void save(LeadAction leadAction);
+}
