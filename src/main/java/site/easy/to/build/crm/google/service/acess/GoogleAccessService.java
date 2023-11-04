4eaba32f6d6883b0d8faa2893e4f83fc4e6865a7
Ahmed Samir <54330098+wp-ahmed@users.noreply.github.com>
2023-11-04 15:17:26 +0200

    Update GoogleAccessService.java

--- src/main/java/site/easy/to/build/crm/google/service/acess/GoogleAccessService.java
+++ src/main/java/site/easy/to/build/crm/google/service/acess/GoogleAccessService.java
@@ -1,5 +1,6 @@
 package site.easy.to.build.crm.google.service.acess;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.security.core.Authentication;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.web.servlet.view.RedirectView;
@@ -12,21 +13,24 @@ import java.util.List;
 
 public interface GoogleAccessService {
 
+
     static final String SCOPE_CALENDAR = "https://www.googleapis.com/auth/calendar";
     static final String SCOPE_GMAIL = "https://www.googleapis.com/auth/gmail.modify";
     static final String SCOPE_DRIVE = "https://www.googleapis.com/auth/drive.file";
-    static final String REDIRECT_URI = "http://localhost:8080/employee/settings/handle-granted-access";
+    static final String REDIRECT_URI = "employee/settings/handle-granted-access";
 
     public RedirectView grantGoogleAccess(Authentication authentication,
                                           HttpSession session,
                                           boolean grantCalendarAccess,
                                           boolean grantGmailAccess,
-                                          boolean grantDriveAccess);
+                                          boolean grantDriveAccess,
+                                          HttpServletRequest request);
     public String handleGrantedAccess(HttpSession session,
                                       String error,
                                       String authCode,
                                       String state,
-                                      Authentication authentication) throws IOException;
+                                      Authentication authentication,
+                                      HttpServletRequest request) throws IOException;
 
     public void verifyAccessAndHandleRevokedToken(OAuthUser oAuthUser, User user, List<String> scopesToCheck) throws IOException;
 
